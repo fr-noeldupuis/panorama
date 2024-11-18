eb2eaa1379fc71154f44ae19f6ed826d19b19b03
Noel Dupuis <noel.dupuis45370@gmail.com>
2024-11-18 13:07:30 +0100

    Initial Commit

---
+++ panorama/panoramaApp.swift
@@ -0,0 +1,17 @@
+//
+//  panoramaApp.swift
+//  panorama
+//
+//  Created by Noel Dupuis on 18/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct panoramaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
