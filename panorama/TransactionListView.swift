5f7e5f52ab0a6d39c4af5225045ab13697fc7f55
Noel Dupuis <noel.dupuis45370@gmail.com>
2024-11-19 12:13:06 +0100

    Setup the sectioned display for the transaction view

--- panorama/TransactionListView.swift
+++ panorama/TransactionListView.swift
@@ -7,29 +7,55 @@
 
 import SwiftUI
 import SwiftData
+import OrderedCollections
 
 struct TransactionListView: View {
     
     @Environment(\.modelContext) private var modelContext
     @Query var transactions: [Transaction]
     
+    // Custom date formatter for the "DD mmmm yyyy" format
+    private let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd MMMM yyyy"
+        return formatter
+    }()
+    
     var body: some View {
-            List {
-                ForEach(transactions) { transaction in
-                    NavigationLink(destination: EditTransactionView(transactionEdited: transaction)) {
-                        Text("Transaction \(transaction.id.uuidString) \(transaction.amount, specifier: "%.2f")")
+        List {
+                        // Group transactions by date
+            ForEach(OrderedDictionary(grouping: transactions, by: \.date).elements, id: \.key) { element in
+                let dailyTransactions = element.value
+                let date = element.key
+                
+                // Calculate the sum of transaction amounts for the day
+                let dailyTotal = dailyTransactions.reduce(0) { $0 + $1.amount }
+                
+                // Define each Section with transactions for that date
+                Section(header: HStack {
+                    Text(dateFormatter.string(from: date))
+                    Spacer()
+                    Text("\(dailyTotal, specifier: "%.2f") €")
+                }
+                ) {
+                    ForEach(dailyTransactions) { transaction in
+                        NavigationLink(destination: EditTransactionView(transactionEdited: transaction)) {
+                            Text("Transaction \(transaction.id.uuidString) \(transaction.amount, specifier: "%.2f")")
+                        }
                     }
                 }
             }
-            .navigationTitle("Transactions")
-            .navigationBarTitleDisplayMode(.large)
-            .toolbar {
-                ToolbarItem(placement: .primaryAction) {
-                    NavigationLink(destination: EditTransactionView()) {
-                        Image(systemName: "plus")
-                    }
+        }
+        .listRowInsets(EdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16)) // Reduce row insets to adjust padding
+        .navigationTitle("Transactions")
+        .navigationBarTitleDisplayMode(.large)
+        .toolbar {
+            ToolbarItem(placement: .primaryAction) {
+                NavigationLink(destination: EditTransactionView()) {
+                    Image(systemName: "plus")
                 }
             }
+        }
     }
 }
 
@@ -51,6 +77,8 @@ struct TransactionListView: View {
     comps2.year = 2022
 
     container.mainContext.insert(Transaction(amount: -20, date: Calendar.current.date(from: comps2)!))
+    container.mainContext.insert(Transaction(amount: -250, date: Calendar.current.date(from: comps2)!))
+    container.mainContext.insert(Transaction(amount: 220, date: Calendar.current.date(from: comps2)!))
     
     container.mainContext.insert(Transaction(amount: 17.54, date: Calendar.current.startOfDay(for: .now)))
     
