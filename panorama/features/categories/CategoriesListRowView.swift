9005fb45ad7a1657baf90dac12ebd4376f3f1812
Noel Dupuis <noel.dupuis45370@gmail.com>
2024-11-27 10:16:00 +0100

    Add category creation

---
+++ panorama/features/categories/CategoriesListRowView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoriesListRowView.swift
+//  panorama
+//
+//  Created by Noel Dupuis on 27/11/2024.
+//
+
+import SwiftUI
+
+struct CategoriesListRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoriesListRowView()
+}
---
+++ panorama/features/categories/EditCategoryView.swift
@@ -0,0 +1,18 @@
+//
+//  EditCategoryView.swift
+//  panorama
+//
+//  Created by Noel Dupuis on 26/11/2024.
+//
+
+import SwiftUI
+
+struct EditCategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditCategoryView()
+}
